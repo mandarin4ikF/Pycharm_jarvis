aa7a88af32e5369c8cdcd52889029a41f2c75c26
mandarin4ikF <alexsandrolobano@gmail.com>
2025-08-11 17:42:17 +0300

    emotion add

--- voice_assistant/emotion.py
+++ voice_assistant/emotion.py
@@ -1,5 +1,26 @@
-from aniemore.recognizers.emotion_recognizer import EmotionRecognizer
+import torch
+from aniemore.recognizers.text import TextRecognizer
+from aniemore.models import HuggingFaceModel
 
-model = EmotionRecognizer()
-result = model.predict("Я очень рад!")
-print(result)
+# Инициализация модели для распознавания эмоций
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = HuggingFaceModel.Text.Bert_Tiny2
+text_recognizer = TextRecognizer(model=model, device=device)
+
+def get_emotion(text):
+    """
+    Возвращает эмоцию текста.
+    """
+    # Возвращаем один наиболее вероятный лейбл (эмоцию)
+    try:
+        emotion = text_recognizer.recognize(text, return_single_label=True)
+        return emotion
+    except Exception as e:
+        print(f"Ошибка распознавания эмоции: {e}")
+        return None
+
+if __name__ == "__main__":
+    # Для проверки запускаем тест
+    test_text = "это работает? :("
+    print(f"Текст: {test_text}")
+    print(f"Эмоция: {get_emotion(test_text)}")
